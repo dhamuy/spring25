c895dcfbb52f48dc17f6533b8014b4f511f0415a
Dylan Hamuy <dylanhamuy@berkeley.edu>
2025-03-06 16:54:27 -0800

    week4 update

--- week4/dylan_hamuy/script.js
+++ week4/dylan_hamuy/script.js
@@ -1,14 +1,25 @@
 function addOneToCounter(){
-    document.getElementById("counter").innerText = parseInt(document.getElementById("counter").innerText) + 1
+    let counter = document.getElementById("counter");
+    let limit = document.getElementById("limit");
+
+    if (parseInt(counter.innerText) < parseInt(limit.innerText)) {
+        counter.innerText = parseInt(counter.innerText) + 1;
+    }
 }
 
-function getWeather(){
-    var apiUrl = "https://api.openweathermap.org/data/3.0/onecall?lat=37.8715&lon=122.2730&appid=7a16c3cbf5f181e57bbe7ebc758c325a"
-    fetch(apiUrl).then(response => {
-        return response.json();
-    }).then(data => {
-        console.log(data)
-    }).catch(err => {
-        console.log("ERROR GETTING WEATHER")
-    });
+function increaseLimit(){
+    let counter = document.getElementById("counter");
+    let limit = document.getElementById("limit");
+    if (parseInt(counter.innerText) >= 5) {
+        limit.innerText = parseInt(limit.innerText) + 5;
+        counter.innerText = parseInt(counter.innerText) - 5;
+    } else {
+        alert("Not enough cookies to increase limit!");
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("limit").innerText = 10;
+    document.getElementById("counter").innerText = 0;
+});
+
